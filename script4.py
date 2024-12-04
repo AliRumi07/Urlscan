3e9c78fae59e9ca6ba37c30d80ce855144118684
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-04 22:50:56 +0500

    Create script4.py

---
+++ script4.py
@@ -0,0 +1,7 @@
+import requests
+import json
+headers = {'API-Key':'6699f474-9f04-4a37-b615-0542cd6a7022','Content-Type':'application/json'}
+data = {"url": "https://foodies-siter.blogspot.com/2024/12/artificial-intelligence_3.html?m=1", "visibility": "public", "country": "fr"}
+response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
+print(response)
+print(response.json())
