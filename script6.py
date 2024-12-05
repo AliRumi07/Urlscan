4c82f8703ec520d588054406f94d9796b7da22c5
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-05 19:25:27 +0500

    Update script6.py

--- script6.py
+++ script6.py
@@ -1,7 +1,7 @@
 import requests
 import json
 headers = {'API-Key':'e8c76f39-847e-41f0-a668-7d1e266f713b','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/12/artificial-intelligence_3.html?m=1", "visibility": "public", "country": "nl"}
+data = {"url": "https://foodies-siter.blogspot.com/2024/12/adcash.html", "visibility": "public", "country": "nl"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
