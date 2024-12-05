fdd368e93325b0fdde37c58d135d96c4f33a0087
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-05 17:15:45 +0500

    Update script14.py

--- script14.py
+++ script14.py
@@ -1,6 +1,6 @@
 import requests
 import json
-headers = {'API-Key':'6699f474-9f04-4a37-b615-0542cd6a7022','Content-Type':'application/json'}
+headers = {'API-Key':'e8c76f39-847e-41f0-a668-7d1e266f713b','Content-Type':'application/json'}
 data = {"url": "https://foodies-siter.blogspot.com/2024/12/artificial-intelligence_3.html?m=1", "visibility": "public", "country": "is"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
