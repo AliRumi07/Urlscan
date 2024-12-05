3a69e392eee151852324679d74d2e4518209d152
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-05 23:15:21 +0500

    Update script17.py

--- script17.py
+++ script17.py
@@ -1,7 +1,7 @@
 import requests
 import json
 headers = {'API-Key':'e8c76f39-847e-41f0-a668-7d1e266f713b','Content-Type':'application/json'}
-data = {"url": "https://entertainment-era.onrender.com/", "visibility": "public", "country": "pl", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://foodies-siter.blogspot.com/2024/11/dunkin-donuts-french-cruller.html", "visibility": "public", "country": "pl", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
