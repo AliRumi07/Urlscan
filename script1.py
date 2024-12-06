cbce3f3dfd3811de67196993145cce324f2f3668
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-06 10:47:48 +0500

    Update script1.py

--- script1.py
+++ script1.py
@@ -1,7 +1,7 @@
 import requests
 import json
-headers = {'API-Key':'e8c76f39-847e-41f0-a668-7d1e266f713b','Content-Type':'application/json'}
-data = {"url": "https://link-target.net/1249104/entertainment", "visibility": "public", "country": "de", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
+headers = {'API-Key':'6699f474-9f04-4a37-b615-0542cd6a7022','Content-Type':'application/json'}
+data = {"url": "https://foodies-siter.blogspot.com/2024/11/apple-muffin.html", "visibility": "public", "country": "de", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
