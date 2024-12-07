1e85ce5209518892a3f53c685e1b1ad07fbb12f0
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-07 18:25:44 +0500

    Delete script5.py

--- script5.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-headers = {'API-Key':'6699f474-9f04-4a37-b615-0542cd6a7022','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/starbucks-blueberry-muffins.html", "visibility": "public", "country": "gb", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
-response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
-print(response)
-print(response.json())
