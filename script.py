cfa469b3ed7033258d298692977f74e35dd5b02f
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-07 20:18:08 +0500

    Update script.py

--- script.py
+++ script.py
@@ -2,154 +2,154 @@ import requests
 import json
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276429375&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
