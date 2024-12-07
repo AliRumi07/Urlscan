ef02ba2cbf143b428ed34559fa57c4f3b101b657
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-07 19:23:41 +0500

    Update script.py

--- script.py
+++ script.py
@@ -2,154 +2,154 @@ import requests
 import json
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/apple-muffin.html", "visibility": "public", "country": "de", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "de", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/starbucks-blueberry-muffins.html", "visibility": "public", "country": "se", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "se", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/huevos-rancheros.html", "visibility": "public", "country": "fi", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "fi", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/ihop-cinn-stack-pancakes.html", "visibility": "public", "country": "dk", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "dk", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/chocolate-pancakes.html", "visibility": "public", "country": "no", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "no", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/krispy-kreme-glazed-donuts.html", "visibility": "public", "country": "is", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "is", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/dunkin-donuts-french-cruller.html", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/old-fashioned-coffee-cake.html", "visibility": "public", "country": "nz", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "nz", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/old-fashioned-coffee-cake.html", "visibility": "public", "country": "pl", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "pl", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/the-madeleine-quiche-lorraine.html", "visibility": "public", "country": "sg", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "sg", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/how-to-boil-eggs.html", "visibility": "public", "country": "pt", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "pt", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/homemade-waffles.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/sweetened-maple-syrup-almond-granola.html", "visibility": "public", "country": "at", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "at", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/easy-fried-cornmeal-mould.html", "visibility": "public", "country": "ch", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "ch", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/cannoli-blueberry-pancakes-ihop-adop.html", "visibility": "public", "country": "il", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "il", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/banana-nut-bread.html", "visibility": "public", "country": "jp", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "jp", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/apple-muffin.html", "visibility": "public", "country": "fr", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "fr", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/starbucks-blueberry-muffins.html", "visibility": "public", "country": "gb", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "gb", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/huevos-rancheros.html", "visibility": "public", "country": "nl", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "nl", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/ihop-cinn-stack-pancakes.html", "visibility": "public", "country": "ca", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "ca", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/chocolate-pancakes.html", "visibility": "public", "country": "it", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "it", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/krispy-kreme-glazed-donuts.html", "visibility": "public", "country": "es", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "https://news-bcetudi.live/tds?id=1276392189&p1=sub1&p2=sub2&p3=sub3&p4=sub4", "visibility": "public", "country": "es", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
