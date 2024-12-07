2043e2c5ce0cad0af995ef5251ec2a980394834a
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-07 18:23:51 +0500

    Delete script14.py

--- script14.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-headers = {'API-Key':'6699f474-9f04-4a37-b615-0542cd6a7022','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/krispy-kreme-glazed-donuts.html", "visibility": "public", "country": "is", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
-response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
-print(response)
-print(response.json())
