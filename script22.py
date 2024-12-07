0223a797bd2f476495ac052bd9d931ead75b8b91
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-07 18:25:14 +0500

    Delete script22.py

--- script22.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-headers = {'API-Key':'6699f474-9f04-4a37-b615-0542cd6a7022','Content-Type':'application/json'}
-data = {"url": "https://foodies-siter.blogspot.com/2024/11/cannoli-blueberry-pancakes-ihop-adop.html", "visibility": "public", "country": "il", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
-response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
-print(response)
-print(response.json())
