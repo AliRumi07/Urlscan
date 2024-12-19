1d397fef56d084c5f136da5a1c229fe36460969b
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-20 01:46:02 +0500

    Update script.py

--- script.py
+++ script.py
@@ -3,154 +3,176 @@ import requests
 import json
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
 headers = {'API-Key':'b5a2f71d-1bb4-45c7-bab2-18ed48287a3a','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+data = {"url": "http://runative-syndicate.com/api/v1/direct/5445275d159142d2a08d883be5748126?
+", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
