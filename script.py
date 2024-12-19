4612276aef40692ef96272b724609bf607f9bbde
AliRumi07 <150955943+AliRumi07@users.noreply.github.com>
2024-12-19 22:54:56 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,156 +1,157 @@
 
+
 import requests
 import json
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/searching.html", "visibility": "public", "country": "de", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "de", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/salt.html", "visibility": "public", "country": "se", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "se", "useragent": "Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36""Mozilla/5.0 (Linux; Android 12; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/free-fire.html", "visibility": "public", "country": "fi", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "fi", "useragent": "Mozilla/5.0 (Linux; Android 13; Samsung SM-G998B) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/faster.html", "visibility": "public", "country": "dk", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "dk", "useragent": "Mozilla/5.0 (Linux; Android 12; OnePlus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/elysium.html", "visibility": "public", "country": "no", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "no", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 7 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/the-pretty-one.html", "visibility": "public", "country": "is", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "is", "useragent": "Mozilla/5.0 (Linux; Android 12; Xiaomi Mi 11) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/the-premium-rush.html", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "au", "useragent": "Mozilla/5.0 (Linux; Android 13; Sony Xperia 1 IV) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/possession-of-hannah-grace.html", "visibility": "public", "country": "nz", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "nz", "useragent": "Mozilla/5.0 (Linux; Android 12; Oppo Find X3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/patient-zero.html", "visibility": "public", "country": "pl", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "pl", "useragent": "Mozilla/5.0 (Linux; Android 13; Vivo X60) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/never-back-down-no-surrender.html", "visibility": "public", "country": "sg", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "sg", "useragent": "Mozilla/5.0 (Linux; Android 12; Huawei P50) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/searching.html", "visibility": "public", "country": "pt", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "pt", "useragent": "Mozilla/5.0 (Linux; Android 13; Motorola Edge 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/salt.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "us", "useragent": "Mozilla/5.0 (Linux; Android 12; Realme GT) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/free-fire.html", "visibility": "public", "country": "at", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "at", "useragent": "Mozilla/5.0 (Linux; Android 14; Asus ROG Phone 6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/faster.html", "visibility": "public", "country": "ch", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "ch", "useragent": "Mozilla/5.0 (Linux; Android 12; Nokia X20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/elysium.html", "visibility": "public", "country": "il", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "il", "useragent": "Mozilla/5.0 (Linux; Android 13; ZTE Axon 30) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/the-pretty-one.html", "visibility": "public", "country": "jp", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "jp", "useragent": "Mozilla/5.0 (Linux; Android 12; LG Velvet) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/the-premium-rush.html", "visibility": "public", "country": "fr", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "fr", "useragent": "Mozilla/5.0 (Linux; Android 14; Google Pixel Fold) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/possession-of-hannah-grace.html", "visibility": "public", "country": "gb", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "gb", "useragent": "Mozilla/5.0 (Linux; Android 12; Lenovo Legion Phone Duel) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/patient-zero.html", "visibility": "public", "country": "nl", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "nl", "useragent": "Mozilla/5.0 (Linux; Android 13; HTC U12+) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/never-back-down-no-surrender.html", "visibility": "public", "country": "ca", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "ca", "useragent": "Mozilla/5.0 (Linux; Android 12; Honor Magic 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/searching.html", "visibility": "public", "country": "it", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "it", "useragent": "Mozilla/5.0 (Linux; Android 13; Xiaomi Redmi Note 12 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
 
 
-headers = {'API-Key':'1d882756-62c6-4319-89e7-e33752f9d896','Content-Type':'application/json'}
-data = {"url": "https://tap-mad.blogspot.com/2024/12/salt.html", "visibility": "public", "country": "es", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
+headers = {'API-Key':'c9e01764-21a3-439d-acbf-9f8a1a3c5e84','Content-Type':'application/json'}
+data = {"url": "https://tap-mad.blogspot.com/2024/12/trafficstars.html", "visibility": "public", "country": "es", "useragent": "Mozilla/5.0 (Linux; Android 14; Pixel 8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36"}
 response = requests.post('https://urlscan.io/api/v1/scan/',headers=headers, data=json.dumps(data))
 print(response)
 print(response.json())
